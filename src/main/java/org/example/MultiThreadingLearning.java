77a64a344cf4456c9ccb9b83b7570b2a81d4751a
RathiAbhi <abhijeetrathi22@gmail.com>
2024-04-27 21:29:35 +0530

    thread using runnable interface

--- src/main/java/org/example/MultiThreadingLearning.java
+++ src/main/java/org/example/MultiThreadingLearning.java
@@ -1,8 +1,17 @@
 package org.example;
 
-public class MultiThreadingLearning {
+public class MultiThreadingLearning implements Runnable {
+
+    @Override
+    public void run(){
+        System.out.println("code executed by thread: "+ Thread.currentThread().getName());
+    }
 
     public static void main(String[] args){
-        System.out.println("Thread Name: "+ Thread.currentThread().getName());
+        System.out.println("Going inside main: "+ Thread.currentThread().getName());
+        MultiThreadingLearning runnableObj = new MultiThreadingLearning();
+        Thread thread = new Thread(runnableObj);
+        thread.start();
+        System.out.println("Finishing the main: "+ Thread.currentThread().getName());
     }
 }
