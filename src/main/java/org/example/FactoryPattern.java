6eec5eaf116b7b7128b51756cea81b4ab0677167
RathiAbhi <abhijeetrathi22@gmail.com>
2024-10-14 10:41:07 +0530

    factory pattern

---
+++ src/main/java/org/example/FactoryPattern.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class FactoryPattern {
+
+    interface Shape(){
+        void draw();
+    }
+
+    class Circle implements Shape{
+
+        @Override
+        public void draw() {
+            System.out.println("Drawing circle");
+        }
+    }
+
+    class ShapeFactory{
+        public Shape getShape(String shapeType){
+            if (shapeType.equals("Circle")){
+                return new Circle();
+            }
+            return null;
+        }
+
+    }
+}
